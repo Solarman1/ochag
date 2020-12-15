b7ccad932d08ef7f74d7d4520e6fa9dfb01b3808
Solarman1 <arthurthehead@gmail.com>
2020-12-15 22:50:54 +0700

    local frontend 2

--- frontend/src/views/Menu/Menu.vue
+++ frontend/src/views/Menu/Menu.vue
@@ -1,14 +1,59 @@
 <template>
-  <v-product-item />
+     <v-card
+    class="mx-auto" 
+    max-width="344" >
+        <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="200px"
+        ></v-img>
+
+        <v-card-title v-text="product.name">
+        Top western road trips
+        </v-card-title>
+
+        <v-card-subtitle>
+        1,000 miles of wonder
+        </v-card-subtitle>
+
+        <v-card-actions>
+        <v-btn text>Share</v-btn>
+
+        <v-btn
+            color="purple"
+            text
+        >
+            Explore
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+            icon
+            @click="show = !show"
+        >
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+        <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text>
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+        </div>
+        </v-expand-transition>
+    </v-card>
 </template>
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
 
-import VProductItem from '../components/v-product-item.vue'
+
 
 export default {
-  components: {VProductItem},
+  components: {},
     name: "Menu",
     props: {},
     data() {
@@ -22,7 +67,7 @@ export default {
         let result = {}
         let vm = this;
         this.PRODUCTS.find(function (item) {
-          if (item.article === vm.$route.query.product) {
+          if (item.categoriId === vm.$route.query.product) {
             result = item;
           }
         })
@@ -35,8 +80,9 @@ export default {
       ]),
     },
     mounted() {
-      if (!this.PRODUCTS.length) {
-        this.GET_PRODUCTS_FROM_API()
+      if(!this.PRODUCTS.length) {
+        this.GET_PRODUCTS_FROM_API();
+        console.log(this.$route.query.product);
       }
       
     }
